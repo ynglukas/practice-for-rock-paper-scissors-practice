bbc0dcac21151afbd55c4417c5c421dfe50291b2
lanre <islukmon@gmail.com>
2024-08-02 12:44:01 +0100

    refactored promptinput(rl) using getCPUmove()

--- game.js
+++ game.js
@@ -45,6 +45,9 @@ function getWinner(move1, move2) {
 
 function getCPUMove() {
   // Your code here
+  const validMoveKeys = Object.keys(VALID_MOVES);
+  const randomIndex = Math.floor(Math.random() * validMoveKeys.length);
+  return validMoveKeys[randomIndex];
 }
 
 function processMove(cmd, cpu) {
@@ -65,9 +68,7 @@ function promptInput(rl) {
       rl.close();
       return;
     } else if (VALID_MOVES[cmd]){
-      const validMoveKeys = Object.keys(VALID_MOVES);
-      const randomIndex = Math.floor(Math.random() * validMoveKeys.length);
-      const cpu = validMoveKeys[randomIndex];
+      const cpu = getCPUMove();
 
       console.log(`You pick ${cmd}, computer picks ${cpu}.`);
       const result = getWinner(cmd, cpu);
